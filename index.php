810cd2135fd8e21f80874a6f89055d860337e542
Cesar Gonzalez <icesar@gmail.com>
2011-05-20 16:26:42 -0600

    Adding directory structure, blank CSS file, and some basic HTML into index file.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="MSSmartTagsPreventParsing" content="true" />
+  <meta name="keywords" content="unreasonable institute, social entrepreneurship, events, conferences, boulder colorado" /> 
+  <meta name="description" content="The Unreasonable Climax Event in Boulder, Colorado" />
+  
+  <title>Unreasonable Climax 2011 — This happens only once a year.</title>
+  <link href="/style.css" media="screen" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+	<h1>Unreasonable Climax 2011</h1>
+	<p>Yes, it will be that awesome.</p>
+
+</body>
+</html>
