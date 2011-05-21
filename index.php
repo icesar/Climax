6524c3e87fb764d70dc5f8f072e68100a412441a
Cesar Gonzalez <icesar@gmail.com>
2011-05-20 18:50:48 -0600

    Navigation and title image done, CSS layout added.

--- index.php
+++ index.php
@@ -1,19 +1,40 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <meta name="MSSmartTagsPreventParsing" content="true" />
-  <meta name="keywords" content="unreasonable institute, social entrepreneurship, events, conferences, boulder colorado" /> 
-  <meta name="description" content="The Unreasonable Climax Event in Boulder, Colorado" />
-  
-  <title>Unreasonable Climax 2011 — This happens only once a year.</title>
-  <link href="/style.css" media="screen" rel="stylesheet" type="text/css" />
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+
+<head profile="http://gmpg.org/xfn/11">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="MSSmartTagsPreventParsing" content="true" />
+	<meta name="keywords" content="unreasonable institute, social entrepreneurship, events, conferences, boulder colorado" /> 
+	<meta name="description" content="The Unreasonable Climax Event in Boulder, Colorado" />
+	
+	<title>Unreasonable Climax 2011 — This happens only once a year.</title>
+	<link href="/style.css" media="screen" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 
-	<h1>Unreasonable Climax 2011</h1>
-	<p>Yes, it will be that awesome.</p>
+	<div class="navigation-wrapper">
+		<div class="navigation">
+			
+			<a class="logo" href="/">Unreasonable</a>
+			
+			<ul id="nav">
+			<li><a class="link-active" href="#first-section" id="about">About the Climax</a></li>
+			<li><a class="" href="#second-section" id="prizes">Register Today</a></li>
+			<li><a class="" href="#third-section" id="experts">Meet the Fellows</a></li>
+			<li><a class="" href="#fourth-section" id="winners">Event Logistics</a></li>
+			<li><a class="" href="#fifth-section" id="signup">Other Events</a></li>
+			</ul>
+			
+		</div>
+	</div>
+
+	<div id="first-section" class="section" data-nav="about">
+		<h1 class="logo">The Unreasonable Institute Presents the Global Climax</h1>
+
+		<p>Yes, it will be that awesome.</p>
+	</div>
+	
 
 </body>
 </html>
